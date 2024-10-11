d4b66faa5880a7be344a159ebfe56254c247f6c1
srikar <srikar.code01@gmail.com>
2024-10-11 19:15:34 +0530

    page2, reload animation and locomotive js

---
+++ 02-retro-website/src/script.js
@@ -0,0 +1,27 @@
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('.total'),
+    smooth: true
+});
+
+const tl = gsap.timeline();
+
+tl.to('.scroll ', {
+    y:'100vh',
+    duration: 0,
+    scale:0.3
+})
+
+tl.to('.scroll ', {
+    y:'-60vh',
+    delay: 1,
+})
+
+tl.to('.scroll ', {
+    rotate: '-360',
+    duration: 1,
+    delay: 1,
+    scale: 1,
+    y:'0vh',
+    ease: "power2.inOut"
+})
