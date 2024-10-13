ba8e2b052385b818e5e98ab208d67b25d60878ef
srikar <srikar.code01@gmail.com>
2024-10-13 21:02:49 +0530

    page3 mid section

--- 02-retro-website/src/script.js
+++ 02-retro-website/src/script.js
@@ -6,22 +6,23 @@ const scroll = new LocomotiveScroll({
 
 const tl = gsap.timeline();
 
-// tl.to('.scroll ', {
-//     y:'100vh',
-//     duration: 0,
-//     scale:0.3
-// })
+tl.to('.scroll ', {
+    y:'100vh',
+    duration: 0,
+    scale:0.3
+})
 
-// tl.to('.scroll ', {
-//     y:'-60vh',
-//     delay: 1,
-// })
+tl.to('.scroll ', {
+    y:'-60vh',
+    delay: 0.7,
+    duration: 1,
+})
 
-// tl.to('.scroll ', {
-//     rotate: '-360',
-//     duration: 1,
-//     delay: 1,
-//     scale: 1,
-//     y:'0vh',
-//     ease: "power2.inOut"
-// })
+tl.to('.scroll ', {
+    rotate: '-360',
+    duration: 1,
+    delay: 1,
+    scale: 1,
+    y:'0vh',
+    ease: "power2.inOut"
+})
