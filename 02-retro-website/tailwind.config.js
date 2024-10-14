264a0fd2828436d55889eedf6aec31687c5b98d3
srikar <srikar.code01@gmail.com>
2024-10-14 10:47:09 +0530

    complete page4

--- 02-retro-website/tailwind.config.js
+++ 02-retro-website/tailwind.config.js
@@ -8,6 +8,13 @@ module.exports = {
         domaineDispCond: ['DomaineDispCondMedium', 'sans-serif'],
         editorialNewLight: ['Editorial New Light', 'sans-serif'],
       },
+      fontWeight: {
+        100: '100',   //thin
+        300: '300',  // Light
+        400: '400',  // Regular
+        600: '600',  // Semi-bold
+        700: '700',  // Bold
+      },
     },
   },
   plugins: [],
