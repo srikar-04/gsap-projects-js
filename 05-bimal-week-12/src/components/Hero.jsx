e0abcd67d5a24c47a297ffc5cbb0e7921b1213da
srikar <srikar.code01@gmail.com>
2025-03-16 08:53:49 +0530

    making hero responsive

--- 05-bimal-week-12/src/components/Hero.jsx
+++ 05-bimal-week-12/src/components/Hero.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import bimalImage from '../images/bimal.png'
 
+// md: 768px -> tablet
+// lg: 1024px -> laptop
+// xl: 1280px -> desktop (generally desktop is 1440px)
+// 2xl: 1536px
+
 function Hero() {
   return (
-    <section className='bg-zinc-500 mx-auto max-w-7xl mt-10'>
+    <section className='bg-zinc-500 mx-auto max-w-7xl mt-10 '>
         <div className='flex flex-col items-center justify-center'>
-            <h1 className='text-6xl font-extrabold tracking-tighter'>
+            <h1 className='text-4xl md:text-6xl font-extrabold tracking-tighter'>
                 <span className='text-orange-950'>Bolo Zubaan</span> <span className='text-orange-50'>Canceri</span>
             </h1>
 
-            <div className='text-xl text-orange-950'>
+            <div className='text-lg md:text-xl text-orange-950'>
                 More you eats, sooner you dies
             </div>
 
